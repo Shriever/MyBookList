ac30a9e98b0058f1b3b1af20dfaf10c813549d03
levi Lieberman <greggreg942@gmail.com>
2020-08-10 07:28:18 -0400

    consolidated static files into html file, added info links to books

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require("http");
+const fs = require("fs");
+
+const overviewHtml = fs.readFileSync(`${__dirname}/Templates/index.html`);
+const overviewCss = fs.readFileSync(`${__dirname}/bootstrap.css`);
 
 const server = http.createServer((req, res) => {
-  res.end("this is the homepage");
+  if (req.url === "/" || req.url === "/overview") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    });
+    res.end(overviewHtml);
+  } else if (req.url === "../bootstrap.css") {
+    res.writeHead(200, { "Content-type": "text/css" });
+    res.end(overviewCss);
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
@@ -12,11 +24,13 @@ server.listen(8000, "127.0.0.1", () => {
 //////////////////////
 //TODO
 // add link to each book in html
-
+DONE
 // create template for book details
+
+// get data from API using URL
 
 // fill in template upon request
 
-// display it to the user
+// display the right one to the user
 
 */
